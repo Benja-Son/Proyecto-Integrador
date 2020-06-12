06d0bb69b7f85105c35c220660e9e77fc493c489
mateo pizzolo <mpizzolo@udesa.edu.ar>
2020-06-12 16:52:25 -0300

    jo

--- Java/home.js
+++ Java/home.js
@@ -8,29 +8,106 @@ window.onload = function(){
     })
     .then(function (data) {
         console.log(data);
-        var tracksrank = document.querySelector('.Ctracks');
-      //  console.log(content);
+        var Ctracks = document.querySelector('.Ctracks');
         var contenido = " ";
         for (let i = 0; i < data.tracks.data.length; i++) {
             var element = data.tracks.data[i];
-            contenido += '<article class= "thetracks">'
-            contenido += '<div class = "thecontainer">'
-            contenido += '<img class="photo" src="' + element.artist.picture + '">'
+                    
+          /*
+            contenido += '<li>'
+            contenido += '<div class="uk-card uk-card-default">'
+            contenido += '<div class="uk-card-media-top">'
+            contenido += '<img src=" '+ element.artist.cover +' "  alt="">'
             contenido += '</div>'
-            contenido += '<img src=" '+ element.album.cover +' " >'
-            contenido += '<h2>;' +element.title+ '</h2>'
-            contenido += '<p>;' +element.artist.name+ '</p>'
-            contenido += '<a class="more" href="Detalle.html"> Learn More </a>'
-            console.log(element.album.cover)
+            contenido += '<div class="uk-card-body">'
+            contenido += '<h3 class="uk-card-title">Headline</h3>'
+            contenido += '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>'
+            contenido += '</div>'
+            contenido += '</div>'
+            contenido += '</li>'
+            */
+            
         }    
         
-        tracksrank.innerHTML = contenido;
+        Ctracks.innerHTML = contenido;
     })
     
-    
-//    .catch(function(error){
-//        console.log("El error fue " + error )
-//    })
-    
-    }
-    
+}
+
+
+
+/* <!--Carrousel Tracks-->
+  <div class="Ctracks" uk-slider="center: true">
+
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
+
+        <ul class="uk-slider-items uk-child-width-1-2@s uk-grid">
+           <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="../Imagenes/thebox.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title">Headline</h3>
+                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="../Imagenes/dontstart.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title">Headline</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="../Imagenes/blinding.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title">Headline</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="../Imagenes/dance monkey.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title">Headline</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+
+            <li>
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="../Imagenes/highestinthe.jpg" alt="">
+                    </div>
+                    <div class="uk-card-body">
+                        <h3 class="uk-card-title">Headline</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
+
+    </div>
+
+    <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+
+</div> */
