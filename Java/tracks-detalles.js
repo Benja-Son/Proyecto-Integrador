4e5cae199fe0539fc744e904852d0963e91b69d5
mateo pizzolo <mpizzolo@udesa.edu.ar>
2020-06-17 18:12:12 -0300

    k

--- Java/tracks-detalles.js
+++ Java/tracks-detalles.js
@@ -22,8 +22,9 @@ window.onload = function(){
             contenido += '<div class="titulo-genero">'
             contenido += '<h1>' +element.title+ '</h1>'
             contenido += '<h1>' +element.artist.name+ '</h1>'
-            contenido += '<img class="photo-genero" href=" '+ element.cover +' " >'
-            contenido += '<p>' + 'la cancion dura: ' +element.duration+ ' minutos' + '</p>'
+            contenido += '<img class="photo-genero" href=" '+ element.album.cover_medium +'" alt="foto" >'
+            var seconds = element.duration / 60
+            contenido += '<p>' + 'la cancion dura: ' +seconds+ ' minutos' + '</p>'
             contenido += '<h1>' +element.album.title+ '</h1>'
 
             //falta boton agregar a playlist 
