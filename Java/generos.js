a1516b0156a2eb371b9fd582ec9a5c1ce2d47472
mateo pizzolo <mpizzolo@udesa.edu.ar>
2020-06-18 22:31:07 -0300

    lh

--- Java/generos.js
+++ Java/generos.js
@@ -13,14 +13,14 @@ window.onload = function(){
     })
     .then(function (data) {
         console.log(data);
-        var tracksrank = document.querySelector('.tracksrank');
+        var tracksrankGenero = document.querySelector('.tracksrankGenero');
         var contenido = " ";
         for (let i = 1; i < data.data.length; i++) {
             var element = data.data[i];
             contenido += '<article class= "thetracks">'
             contenido += '<div class = "overlay">'
             contenido += '<a href="../HTML/generos-detalle.html?idDetalle='+element.id+'">'
-            contenido += '<img class="image" src="' + element.picture_medium + '">'
+            contenido += '<img class="imageGenero" src="' + element.picture_medium + '">'
             contenido += '</div>'
             contenido += '<h2 class="gTitle">' +element.name+ '</h2>'
             contenido += '</a>'
@@ -31,7 +31,7 @@ window.onload = function(){
         }    
         
         
-        tracksrank.innerHTML = contenido;
+        tracksrankGenero.innerHTML = contenido;
     })
       
     .catch(function(error){
